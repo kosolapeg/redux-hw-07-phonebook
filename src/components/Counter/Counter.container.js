f8852f764447a87ebe6f7fcc0ba7b294fa72a461
Grigor Bitny <kosolapeg@gmail.com>
2021-12-03 19:05:19 +0100

    done

--- src/components/Counter/Counter.container.js
+++ src/components/Counter/Counter.container.js
@@ -11,24 +11,3 @@ const mapDispatchToProps = dispatch => ({
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(CounterButton);
-
-// import Counter from './Counter';
-// import { connect } from 'react-redux';
-// import increment from '../../redux/counter';
-
-// const mapStateToProps = state => {
-//   console.log('STATESHHHIIIIIITTT', state);
-//   return {
-//     clicks: state.counter.value,
-//   };
-// };
-
-// // const mapDispatchToProps = dispatch => ({
-// //   onClick: () => dispatch(increment()),
-// // });
-
-// const mapDispatchToProps = dispatch => ({
-//   onClick: () => dispatch(increment()),
-// });
-
-// export default connect(mapStateToProps, mapDispatchToProps)(Counter);
